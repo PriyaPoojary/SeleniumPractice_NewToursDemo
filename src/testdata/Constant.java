ca6c4482a2de7056cae880bc07a02abf4a408c18
491784 <491784@PC260018.cts.com>
2018-01-23 11:15:41 +0530

    Test Data Path Updated

--- src/testdata/Constant.java
+++ src/testdata/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 	
 	public static final String Driver_Path= "D:\\SELENIUM\\Drivers\\chromedriver.exe";
 	public static final String Url = "http://newtours.demoaut.com";
-    public static final String TestData_Path = "D:\\Priya\\SeleniumPractice\\SeleniumDemo\\src\\testdata\\";
+    public static final String TestData_Path = "D:\\Priya\\SeleniumPractice\\NewToursDemo\\src\\testdata\\";
     public static final String TestData_File = "TestData.xlsx";
 
 }
